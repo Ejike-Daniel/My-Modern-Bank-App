30ae2207e4c3de0d1401644c1b873c3a5df052ab
Elrey009 <daniloejike@gmail.com>
2024-10-20 20:16:47 +0100

    modified files

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,8 @@
-
-
-export default function Button() {
+export default function Button({ styles }) {
   return (
-    <div>
-      
-    </div>
-  )
+    <button
+      type="button"
+      className={` py-4 px-6 bg-blue-gradient font-poppins rounded-lg font-medium text-primary text-[18px] outline-none ${styles}`}
+    >Get Started</button>
+  );
 }
