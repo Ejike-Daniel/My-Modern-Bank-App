df04a787b7803f436aa390afd5dd7ef6c7c346d0
Elrey009 <daniloejike@gmail.com>
2024-10-21 09:13:59 +0100

    modified files

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,9 +1,31 @@
-
+import styles from "../style";
+import { feedback } from "../constants";
+import FeedbackCard from "./FeedbackCard";
 
 export default function Testimonials() {
   return (
-    <div>
-      
-    </div>
-  )
+    <section
+      id="client"
+      className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}
+    >
+      {/* todo */}
+      <div className="absolute z-0 w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient" />
+      <div className="realtive z-[1] mb-6 md:flex-row flex-col sm:mb-16 flex items-center justify-between w-full ">
+        <h1 className={styles.heading2}>
+          What people are <br className="sm:block hidden" /> saying about us{" "}
+        </h1>
+        <div>
+          <p className={`${styles.paragraph} text-left max-w-[450px]`}>
+            Everything you need to accept card payments and grow your business
+            anywhere on the planet.
+          </p>
+        </div>
+      </div>
+      <div className=" flex flex-wrap realtove z-[1] justify-center w-full feedback-container sm:justify-start ">
+        {feedback.map((card) => (
+          <FeedbackCard key={card.id} {...card} />
+        ))}
+      </div>
+    </section>
+  );
 }
